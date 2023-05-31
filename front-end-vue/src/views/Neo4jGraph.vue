1e895ef56cea8716f8dabcfc8c67322bf8e00598
Linghan-z <zhanglinghan01@outlook.com>
2023-05-31 21:33:44 +0800

    neovis展示neo4j图谱，并设计了查询

--- front-end-vue/src/views/Neo4jGraph.vue
+++ front-end-vue/src/views/Neo4jGraph.vue
@@ -1,19 +1,44 @@
 <template>
   <div class="myDiv">
-    <div ref="viz" id="viz"></div>
+    <div>
+      <span class="mr-5 ml-5">按类型查询</span>
+      <el-cascader
+          class="ml-5"
+          :options="options"
+          :props="{ checkStrictly: true }"
+          clearable
+          @change="generateCypher"></el-cascader>
+      <el-input-number class="ml-5" v-model="limitNum" clearable placeholder="限制数量" style="width: 100px"
+                       @blur="submit"></el-input-number>
+      <el-button class="ml-5" type="primary" @click="submit">搜索</el-button>
+      <el-button class="ml-5" type="text" @click="stabilize">Stabilize</el-button>
+    </div>
+    <div>
+      <span class="ml-5" style="margin-right: 20px">实体名称</span>
+<!--      <el-select class="ml-5" v-model="from" placeholder="请选择" style="width: 150px">-->
+<!--        <el-option-->
+<!--            v-for="item in labels"-->
+<!--            :key="item.value"-->
+<!--            :label="item.label"-->
+<!--            :value="item.value"-->
+<!--            @change="changeFrom">-->
+<!--        </el-option>-->
+<!--      </el-select>-->
+      <el-input class="ml-5" v-model="searchValue" type="String" clearable placeholder="搜索内容" style="width: 150px"
+                @change="valueSearch"></el-input>
+      <el-button class="ml-5" type="primary" @click="submit">搜索</el-button>
+    </div>
+    <div id="viz"></div>
     Cypher query: <textarea rows="4" cols=50 id="cypher"></textarea><br>
-    <input type="submit" value="Submit" id="reload" @click="submit">
-    <input type="submit" value="Stabilize" id="stabilize" @click="stabilize">
   </div>
 </template>
 
 <script>
 // 引入jquery.js
-// import $ from 'jquery';
+import $ from 'jquery';
 
 // 引入neovis.js
-import Neovis from 'neovis.js';
-// import 'vis-network/dist/dist/vis-network.min.css';
+import NeoVis from '../utils/neovis.js'
 
 export default {
   name: "Neo4jGraph",
@@ -21,60 +46,341 @@ export default {
   props: {},
   data() {
     return {
-      viz: {} //定义一个viz对象
+      viz: {},  //定义一个viz对象
+      options: [{
+        value: 'Organization',
+        label: '组织',
+        children: [{
+          value: 'organization_has_area',
+          label: '目标地区',
+          children: [{
+            value: 'Area',
+            label: '地区'
+          }]
+        }, {
+          value: 'organization_from',
+          label: '发源地',
+          children: [{
+            value: 'Area',
+            label: '地区'
+          }]
+        }, {
+          value: 'organization_has_industry',
+          label: '目标行业',
+          children: [{
+            value: 'Industry',
+            label: '行业'
+          }]
+        }, {
+          value: 'organization_has_attacktype',
+          label: '攻击方式',
+          children: [{
+            value: 'Attacktype',
+            label: '攻击方式'
+          }]
+        }, {
+          value: 'organization_has_domain',
+          label: '拥有域名',
+          children: [{
+            value: 'Domain',
+            label: '域名'
+          }]
+        }, {
+          value: 'organization_has_ip',
+          label: 'IP',
+          children: [{
+            value: 'Ip',
+            label: 'IP'
+          }]
+        }, {
+          value: 'organization_has_sha256',
+          label: 'Sha256',
+          children: [{
+            value: 'Sha256',
+            label: 'Sha256'
+          }]
+        }]
+      }, {
+        value: 'Area',
+        label: '地区',
+        children: [{
+          value: 'area_targeted_by_org',
+          label: '被攻击',
+          children: [{
+            value: 'Organization',
+            label: '组织'
+          }]
+        }, {
+          value: 'area_has_organization',
+          label: '发源地',
+          children: [{
+            value: 'Organization',
+            label: '组织'
+          }]
+        }]
+      }, {
+        value: 'Industry',
+        label: '行业',
+        children: [{
+          value: 'industry_targeted_by_org',
+          label: '被攻击',
+          children: [{
+            value: 'Organization',
+            label: '组织'
+          }]
+        }]
+      }, {
+        value: 'Attacktype',
+        label: '攻击方式',
+        children: [{
+          value: 'attacktype_used_by_org',
+          label: '被组织使用',
+          children: [{
+            value: 'Organization',
+            label: '组织'
+          }]
+        }]
+      }, {
+        value: 'Ip',
+        label: 'IP',
+        children: [{
+          value: 'ip_possessed_by_org',
+          label: '所属组织',
+          children: [{
+            value: 'Organization',
+            label: '组织'
+          }]
+        }]
+      }, {
+        value: 'Domain',
+        label: 'Domain',
+        children: [{
+          value: 'domain_belongs_to_org',
+          label: '所属组织',
+          children: [{
+            value: 'Organization',
+            label: '组织'
+          }]
+        }]
+      }, {
+        value: 'Sha256',
+        label: 'Sha256',
+        children: [{
+          value: 'sha256_possessed_by_org',
+          label: '所属组织',
+          children: [{
+            value: 'Organization',
+            label: '组织'
+          }]
+        }]
+      }],
+      // labels: [{
+      //   value: 'Organization',
+      //   label: '组织'
+      // }, {
+      //   value: 'Area',
+      //   label: '地区'
+      // }, {
+      //   value: 'Industry',
+      //   label: '行业'
+      // }, {
+      //   value: 'Attacktype',
+      //   label: '攻击方式'
+      // }, {
+      //   value: 'Ip',
+      //   label: 'IP'
+      // }, {
+      //   value: 'Domain',
+      //   label: 'Domain'
+      // }, {
+      //   value: 'Sha256',
+      //   label: 'Sha256'
+      // }],
+      values: [],
+      cypher: '',
+      from: '',
+      relation: '',
+      to: '',
+      limitNum: 50,
+      searchValue: '',
+      searchValueCypher: '',
+      searchLabel: '',
+      limit: ''
     }
   },
   mounted() {
-      this.draw();
+    this.draw();
   }, //渲染
   methods: {
+    resetData() {
+      this.searchValueCypher = ''
+      this.searchLabel = ''
+    },
     submit() {
-      var cypher = $("#cypher").val();
-      if (cypher.length > 3) {
-        this.viz.renderWithCypher(cypher);
+      // var cypher = $("#cypher").val();
+      this.valueSearch(this.searchValue)
+      this.generateCypher(this.values, this.limitNum)
+      this.viz.clearNetwork();
+      if (this.cypher.length > 3) {
+        this.viz.renderWithCypher(this.cypher);
       } else {
         console.log("reload");
         this.viz.reload();
-
       }
+      // this.resetData()
     },
     stabilize() {
       this.viz.stabilize();
     },
     draw() {
-      var config = {
-        container_id: "viz",
-        server_url: "bolt://localhost:7687",
-        server_user: "neo4j",
-        server_password: "123456",
-        labels: {
-          //"Character": "name",
-          "Area": {
-            "caption": "name",
-            "size": "pagerank",
-            "community": "community"
-            //"sizeCypher": "MATCH (n) WHERE id(n) = {id} MATCH (n)-[r]-() RETURN sum(r.weight) AS c"
+      let config = {
+        containerId: "viz",
+        neo4j: {
+          serverUrl: "bolt://localhost:7687",
+          serverUser: "neo4j",
+          serverPassword: "123456"
+        },
+        visConfig: {
+          nodes: {},
+          edges: {
+            arrows: {
+              to: {enabled: true}
+            }
           },
-          "Organization": {
-            "caption": "name",
-            "size": "pagerank",
-            "community": "community"
+        },
+        labels: {
+          Area: {
+            label: "name",
+          },
+          Organization: {
+            label: "name",
+          },
+          Industry: {
+            label: "industry",
+          },
+          Attacktype: {
+            label: "attacktype",
+          },
+          Ip: {
+            label: "value",
+          },
+          Domain: {
+            label: "value",
+          },
+          Sha256: {
+            label: "value",
           }
         },
         relationships: {
-          "organization_from": {
-            "thickness": "weight",
-            "caption": false
+          organization_form: {
+            value: "weight"
+          },
+          area_targeted_by_org: {
+            value: "weight"
+          },
+          organization_has_area: {
+            value: "weight"
+          },
+          attacktype_used_by_org: {
+            value: "weight"
+          },
+          organization_has_attacktype: {
+            value: "weight"
+          },
+          domain_belongs_to_org: {
+            value: "weight"
+          },
+          organization_has_domain: {
+            value: "weight"
+          },
+          industry_targeted_by_org: {
+            value: "weight"
+          },
+          organization_has_industry: {
+            value: "weight"
+          },
+          ip_possessed_by_org: {
+            value: "weight"
+          },
+          organization_has_ip: {
+            value: "weight"
+          },
+          sha256_possessed_by_org: {
+            value: "weight"
+          },
+          organization_has_sha256: {
+            value: "weight"
+          },
+          area_has_organization: {
+            value: "weight"
           }
         },
-        //查询节点的语句，写成你们的
-        initial_cypher: "match (n)-[r]->(m) return n,r,m;"
-
+        initialCypher: 'MATCH (n)-[r:organization_from]->(m) RETURN n,r,m'
       };
-
       this.viz = new NeoVis(config);
       this.viz.render();
       console.log(this.viz);
+    },
+    generateCypher(value) {
+      this.values = value;
+      let length = this.values.length
+      console.log(this.values)
+      this.from = ''
+      this.relation = ''
+      this.to = ''
+      this.limit = ''
+      let from = ''
+      let relation = ''
+      let to = ''
+      if (length > 0) {
+        this.from = this.values[0]
+        from = ':' + this.from
+      }
+      if (length > 1) {
+        this.relation = this.values[1]
+        relation = ':' + this.relation
+      }
+      if (length > 2) {
+        this.to = this.values[2]
+        to = ':' + this.to
+      }
+      if (this.limitNum === undefined) {
+        this.limit = ''
+      } else {
+        this.limit = ' LIMIT ' + this.limitNum
+      }
+      console.log(this.limitNum)
+      console.log(this.limit)
+      this.cypher = 'MATCH (n' + from + ')-[r' + relation + ']->(m' + to + ')' + this.searchValueCypher + 'RETURN n,r,m' + this.limit
+      console.log(this.cypher)
+    },
+    valueSearch(searchValue) {
+      this.searchValue = searchValue
+      if (this.searchValue !='') {
+        this.changeFrom()
+        this.searchValueCypher = ' WHERE n.' + this.searchLabel + '="' + this.searchValue + '" '
+      } else {
+        this.searchValueCypher = ''
+      }
+
+      console.log('======' + this.searchValueCypher)
+    },
+    changeFrom() {
+      this.searchLabel =''
+      this.searchValueCypher = ''
+      console.log('======' +this.searchValue)
+      console.log('======' +this.searchLabel)
+      if (this.searchLabel === '') {
+        if (this.from === 'Organization' || this.from === 'Area') {
+          this.searchLabel = 'name'
+        } else if (this.from === 'Industry') {
+          this.searchLabel = 'industry'
+        } else if (this.from === 'Attacktype') {
+          this.searchLabel = 'attacktype'
+        } else {
+          this.searchLabel = 'value'
+        }
+      }
     }
   },
 }
@@ -83,20 +389,23 @@ export default {
 <style lang="scss">
 
 .myDiv {
-  width: 800px;
+  width: 100vw;
   height: 800px;
 }
+
 textarea {
   border: 1px solid lightgray;
   margin: 5px;
   border-radius: 5px;
 }
+
 #viz {
   width: 100%;
   height: 80%;
   border: 1px solid #f1f3f4;
   font: 22pt arial;
 }
+
 input {
   border: 1px solid #ccc;
 }
